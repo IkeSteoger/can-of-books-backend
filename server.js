87935b4da7e9631435181636ab5b9c713f82ea47
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-04-24 15:02:31 -0700

    repo setup with some code changes and readme update

--- server.js
+++ server.js
@@ -3,11 +3,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`));
+
+mongoose.connect(process.env.DB_URL);
+
+// *** BELOW CODE FOR TROUBLESHOOTING MongoDB IN TERMINAL ***
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
 
 app.get('/test', (request, response) => {
 
