39222177c7d1b2747cfaffe651f3bff3aabeedc7
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-04-25 12:26:06 -0700

    lab12 backend goodies

--- clear.js
+++ clear.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL);
 
-const Book = require('./models/cat');
+const Book = require('./models/book');
 
 async function clear() {
   try {
     await Book.deleteMany({});
-    console.log('books cleared from DB');
+    console.log('Books cleared from DB');
   } catch (err) {
     console.error(err);
   } finally {
